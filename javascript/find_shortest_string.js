c63b355defd211b58c93e45811bca51f5f1dfc9f
Ben Cavins <ben.cavins@flatironschool.com>
2023-02-15 16:31:57 -0500

    do prob

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,20 +1,32 @@
+
+// Time complexity:   O(n)
+// Space complexity:  O(1)
 function findShortestString(arr) {
-  // type your code here
+  let smallest = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length < smallest.length) {
+      smallest = arr[i]
+    }
+  }
+  return smallest
 }
 
 if (require.main === module) {
-  // add your own tests in here
-  console.log("Expecting: 'a'");
-  console.log("=>", findShortestString(['aaa', 'a', 'bb', 'ccc']));
+  console.log(findShortestString(['aaa', 'a', 'bb', 'ccc']))
 
-  console.log("");
 
-  console.log("Expecting: 'hi'");
+  // // add your own tests in here
+  // console.log("Expecting: 'a'");
+  // console.log("=>", findShortestString(['aaa', 'a', 'bb', 'ccc']));
+
+  // console.log("");
+
+  // console.log("Expecting: 'hi'");
   console.log("=>", findShortestString(['cat', 'hi', 'dog', 'an']));
 
-  console.log("");
+  // console.log("");
 
-  console.log("Expecting: 'lily'");
+  // console.log("Expecting: 'lily'");
   console.log("=>", findShortestString(['flower', 'juniper', 'lily', 'dadelion']));
 
   // BENCHMARK HERE
